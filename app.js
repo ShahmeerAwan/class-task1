41a615b1a5c3fbbb6986b421ef151ebfe9f2e636
ShahmeerAwan <shahmeer90p@gmail.com>
2025-02-04 09:31:37 +0500

    class

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Error!please enter a valid password")
+alert("Welcome to JS land...\n Happy coding!")
+alert("Welcome to JS Land ")
+alert("Happy Coding")
+alert("Hello... I can run JS through my web browsers")
